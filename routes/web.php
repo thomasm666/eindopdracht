76cc7898b90f91817f5ddee4cae9d6b397cadb36
Thomas Moed <t.moed@student.alfa-college.nl>
2019-01-21 10:54:21 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+route::get('/about', function(){
+   return view('<h1>pages.about</h1>');
+});
